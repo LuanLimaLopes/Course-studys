65e1c823f03e0a63f8ecb8ed0bfd4f87b64be698
LuanLopes3L <luandodds543@gmail.com>
2025-08-07 00:52:31 -0300

    add: Arquivo modificado.

--- POO_advanced/mixins.py
+++ POO_advanced/mixins.py
@@ -7,3 +7,38 @@ class HtmlToStringMixin:
             .replace('(', '<strong>(') \
             .replace(')', ')</strong>')
         return f'<span>{html}</span>'
+    
+
+class People:
+    def  __init__(self,name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+    
+
+class Animal:
+    def __init__(self,name, pet=True):
+        self.name = name
+        self.pet = pet
+    
+    def __str__(self):
+        return self.name + ' (pet)' if self.pet else ''
+    
+class PeopleHtml(HtmlToStringMixin, People):
+    pass
+
+
+class AnimalHtml(HtmlToStringMixin, Animal):
+    pass
+
+
+if __name__ == '__main__':
+    p1 = People('Lavinia poloni')
+    p2 = PeopleHtml('Luan lopes')
+    print(p1)
+    print(p2)
+
+    Luna = AnimalHtml('Luna')
+    print(Luna)
+
